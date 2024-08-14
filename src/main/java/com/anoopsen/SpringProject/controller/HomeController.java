2ece552cd6465fbd3ad17cff948a0b2c2bde66a1
anoop675 <anoopsen2003@gmail.com>
2024-08-14 13:55:21 +0530

    Third commit

--- src/main/java/com/anoopsen/SpringProject/controller/HomeController.java
+++ src/main/java/com/anoopsen/SpringProject/controller/HomeController.java
@@ -1,11 +1,16 @@
 package com.anoopsen.SpringProject.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.anoopsen.SpringProject.model.Category;
+import com.anoopsen.SpringProject.model.Product;
 import com.anoopsen.SpringProject.service.CategoryService;
 import com.anoopsen.SpringProject.service.ProductService;
 
@@ -19,17 +24,28 @@ public class HomeController {
 	@Autowired
 	ProductService product_service;
 	
-	@GetMapping(value={"/", "/home"})
+	//@GetMapping(value={"/", "/home"})
+	@GetMapping(value="/home")
 	public String home(Model model) {
 		return "index";
 	}
 	
-	@GetMapping("/shop")
+	@GetMapping(value="/shop")
 	public String shop(Model model) {
 		model.addAttribute("categories", category_service.getAllCategory());
 		model.addAttribute("products", product_service.getAllProduct());
 		
 		return "shop";
 	}
+	
+	@GetMapping(value="/shop/category/{id}")
+	public String shopByCategory(@PathVariable int id, Model model) {
+		List<Category> categories = category_service.getAllCategory();
+		List<Product> productsByCategory = product_service.getAllProductByCategoryId(id);
 		
+		model.addAttribute("categories", categories);
+		model.addAttribute("products", productsByCategory);
+		
+		return "shop";
+	}
 }
